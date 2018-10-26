0a2ec03700a83233edac6137843cd4ac398d3d8d
chenya <1123236324@qq.com>
2018-10-26 14:17:28 +0800

    添加产品

--- prototype/baseComponent.js
+++ prototype/baseComponent.js
@@ -7,7 +7,9 @@ import gm from 'gm'
 
 export default class BaseComponent {
 	constructor(){
-
+		this.idList = ['restaurant_id', 'food_id', 'orderId', 'user_id', 'address_id', 'cart_id', 'img_id', 'category_id', 'item_id', 'sku_id'];
+		this.imgTypeList = ['shop', 'food', 'avatar','default'];
+		this.uploadImg = this.uploadImg.bind(this)
 	}
 	async fetch(url = '', data = {}, type = 'GET', resType = 'JSON'){
 		type = type.toUpperCase();
@@ -46,15 +48,14 @@ export default class BaseComponent {
 				responseJson = await response.json();
 			}
 		} catch (error) {
-			console.error(error)
+			console.log('获取http数据失败');
 			throw new Error(error)
 		}
 		return responseJson
 	}
 	//获取id列表
 	async getId(type){
-		const typeList = ['orderId', 'userId', 'addressId', 'cartId', 'imgId'];
-		if (!typeList.includes(type)) {
+		if (!this.idList.includes(type)) {
 			console.log('id类型错误');
 			throw new Error('id类型错误');
 			return
@@ -65,45 +66,59 @@ export default class BaseComponent {
 			await idData.save();
 			return idData[type]
 		}catch(err){
+			console.log('获取ID数据失败');
 			throw new Error(err)
 		}
 	}
 
-	async uploadImg(req, type = 'default'){
-		return new Promise((resolve, reject) => {
-			const form = formidable.IncomingForm();
-			form.uploadDir = './public/img/' + type;
-			form.parse(req, async (err, fields, files) => {
-				let imgId;
-				try{
-					imgId = await this.getId('imgId');
-				}catch(err){
-					console.log('获取图片id失败');
-					fs.unlink(files.file.path)
-					reject(err);
-				}
-				const imgUrl = new Date().getTime().toString() + imgId;
-				const extname = path.extname(files.file.name);
-				const repath = './public/img/' + type + '/' + imgUrl + extname;
-				try{
-					await fs.rename(files.file.path, repath);
-					gm(repath)
-					.resize(400, 400, '!')
-					.write(repath, async (err) => {
-						if(err){
-							console.log('改写图片尺寸失败');
-							fs.unlink(repath);
-							reject(err);
-						}else{
-							resolve(repath.replace(/^\.\/public/, ''));
-						} 
-					})
-				}catch(err){
-					console.log('改写图片路径失败');
-					fs.unlink(files.file.path)
-					reject(err);
-				}
-			});
-		})
+	async uploadImg(req, res, next){
+		const type = req.params.type;
+		if (!this.imgTypeList.includes(type)) {
+			console.log('前台传入参数错误');
+			res.send({
+				status: 0,
+				type: 'ERROR_PARAMS',
+				message: '参数错误',
+			})
+			return
+		}
+		const form = formidable.IncomingForm();
+		form.uploadDir = './public/img/' + type;
+		
+		form.parse(req, async (err, fields, files) => {
+			let img_id;
+			try{
+				img_id = await this.getId('img_id');
+			}catch(err){
+				console.log('获取图片id失败');
+				fs.unlink(files.file.path)
+				reject(err);
+			}
+			const imgUrl = (new Date().getTime() + Math.ceil(Math.random()*10000)).toString(16) + img_id;
+			const extname = path.extname(files.file.name);
+			const repath = './public/img/' + type + '/' + imgUrl + extname;
+			try{
+				await fs.rename(files.file.path, repath);
+				gm(repath)
+				.resize(400, 400, '!')
+				.write(repath, async (err) => {
+					if(err){
+						console.log('改写图片尺寸失败');
+						fs.unlink(repath);
+						reject(err);
+					}else{
+						const path = repath.replace(/^\.\/public/, '');
+						res.send({
+							status: 1,
+							image_path: path
+						})
+					} 
+				})
+			}catch(err){
+				console.log('改写图片路径失败');
+				fs.unlink(files.file.path)
+				reject(err);
+			}
+		});
 	}	
 }
--- routes/shopping.js
+++ routes/shopping.js
@@ -2,9 +2,15 @@
 
 import express from 'express';
 import Shop from '../controller/shopping/shop'
+import Food from '../controller/shopping/food'
+import BaseComponent from '../prototype/baseComponent'
+const baseHandle = new BaseComponent();
 const router = express.Router();
 
 router.post('/addshop', Shop.addShop);
-router.post('/addimg', Shop.uploadShopImg);
+router.post('/addimg/:type', baseHandle.uploadImg);
+router.post('/addfood', Food.addFood);
+router.get('/getcategory/:restaurant_id', Food.getCategory);
+router.post('/addcategory', Food.addCategory);
 
 export default router
